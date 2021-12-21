5814644a44002aa8e5a33976e8ef6960c750e123
Ashish Pondit <ashishpondit@gmail.com>
2021-12-22 00:09:05 +0600

    addPost with add image option
    no complete design but all the options are working.

--- app/components/PostComp.js
+++ app/components/PostComp.js
@@ -57,10 +57,6 @@ const styles = StyleSheet.create({
       borderRadius: 5,
       flexDirection: 'row',
       alignItems: 'center',
-      
-      
-      
-    
   },
 
   
--- app/screens/AddPost.js
+++ app/screens/AddPost.js
@@ -1,45 +1,19 @@
 
 
-import * as React from 'react';
-import { View, Text, Button, StyleSheet,TextInput} from 'react-native';
+// import * as React from 'react';
+import React, { useEffect } from 'react';
+import { View, Text, Button, StyleSheet,TextInput, Platform, StatusBar, Image, TouchableHighlight} from 'react-native';
 import imagePicker from 'react-native-image-picker';
 import { Picker } from '@react-native-picker/picker';
+import { Entypo } from '@expo/vector-icons';
+
+// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+// import { Avatar, Button as BtnP } from 'react-native-paper';
+import * as ImagePicker from 'expo-image-picker';
 
 import colors from '../config/colors';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-
-
-// class AddPost extends React.Component {
-//     handleChoosePhoto = () => {
-//         const options = {
-//             noData: true,
-//         }
-//         imagePicker.launchImageLibrary(options,response => {
-//             console.log('response',response)
-//         })
-//     }
-
-//     render(){
-//         return (
-//             <View style={styles.postContainer}>
-//                 <Text style={{
-//                     width: '100%',
-//                     height: 30,
-//                     justifyContent: 'center',
-//                     backgroundColor: 'green',
-//                     alignItems: 'center',
-//                     fontSize: 20
-//                 }}>
-//                     Add New Post
-//                 </Text>
-//                 <Button title='Add Image' 
-//                     onPress={this.handleChoosePhoto}/>
-//             </View>
-            
-//         )
-
-//     }
-// }
 
 const AddPost = () => {
     const [title, tileChangeText] = React.useState(null);
@@ -47,6 +21,33 @@ const AddPost = () => {
     const [description, descriptionChangeText] = React.useState(null);
 
     const [pickValue,setPickValue] = React.useState('Action')
+    const [image, setImage] = React.useState(null);
+
+        useEffect(() => {
+            (async () => {
+              if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                  alert('Sorry, we need camera roll permissions to make this work!');
+                }
+              }
+            })();
+          }, []);
+        
+          const pickImage = async () => {
+            let result = await ImagePicker.launchImageLibraryAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.All,
+              allowsEditing: true,
+              aspect: [4, 3],
+              quality: 1,
+            });
+        
+            console.log(result);
+        
+            if (!result.cancelled) {
+              setImage(result.uri);
+            }
+          };
 
     return (
         <View style={styles.postContainer}>
@@ -60,7 +61,25 @@ const AddPost = () => {
             }}>
                 Add New Post
             </Text>
-            <Button title='Add Image'/>
+
+            
+            {/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Button title="Pick an image from camera roll" onPress={pickImage} />
+                {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            </View> */}
+
+            <TouchableOpacity onPress={pickImage}
+                style={{
+                    height: 150,
+                    width: 150,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#d4d99e'
+                }}>
+                <Entypo name="camera" size={24} color="black" />
+            </TouchableOpacity>
+
+          
 
             <TextInput
                 onChangeText={tileChangeText} placeholder='Title' style={styles.inputTextDesign} value={title}/>
@@ -82,7 +101,9 @@ const AddPost = () => {
                 <Picker.Item label='Comedy' value='Comedy'/>
             </Picker>
 
-            <Button title='Add Book' onPress={()=> {console.log(pickValue)}}/>
+            <Button title='Add Book' onPress={()=> {console.log(title,author,description,pickValue,image)}}/>
+
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
         </View>
         
     )
@@ -97,6 +118,7 @@ const styles = StyleSheet.create({
         borderRadius: 0,
         flexDirection: 'column',
         alignItems: 'center',
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     },
 
     inputTextDesign: {
